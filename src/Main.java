6adc02009b75430dda07f374317773c76595c661
Betinsk <gmdust@hotmail.com>
2020-04-03 22:09:22 -0300

    Uri 1012

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,23 @@ public class Main {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
 		double pi = 3.14159;
 		
-		double raio = sc.nextDouble();
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		double C = sc.nextDouble();
 		
-		double volume = (4/3.0) * pi * Math.pow(raio, 3);
-		System.out.printf("VOLUME = %.3f%n", volume);
+		double trianguloRetangulo = A * C /2; 
+		double circuloRaio = pi * Math.pow(C, 2);
+		double areaTrapezio = (A + B) / 2 * C;
+		double areaQuadrado = B * B;
+		double areaRetangulo = A * B;
 		
+		System.out.printf("TRIANGULO: %.3f%n", trianguloRetangulo);
+		System.out.printf("CIRCULO: %.3f%n", circuloRaio);
+		System.out.printf("TRAPEZIO: %.3f%n", areaTrapezio);
+		System.out.printf("QUADRADO: %.3f%n", areaQuadrado);
+		System.out.printf("RETANGULO: %.3f%n", areaRetangulo);
 	}
 }
