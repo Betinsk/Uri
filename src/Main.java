8fdd983a9eb7ddf0d542ac5b2b9d35270eca4996
Betinsk <gmdust@hotmail.com>
2020-04-05 20:00:17 -0300

    Uri 1016

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,9 @@ public class Main {
 		
 	Locale.setDefault(Locale.US);
 	Scanner sc = new Scanner(System.in);
-	
-	double x1 = sc.nextDouble();
-	double y1 = sc.nextDouble();
-	double x2 = sc.nextDouble();
-	double y2 = sc.nextDouble();
-	
-	double distancia = Math.sqrt(Math.pow((x2-x1), 2) + (Math.pow(y2-y1, 2)));
 		
-	System.out.printf("%.4f%n", distancia);
+	int x = sc.nextInt();
+	int minutos = x * 2;
+		System.out.printf("%d minutos\n", minutos);
 	}
 }
