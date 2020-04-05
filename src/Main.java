07500a1cd98c0d387a63df2fcfb67fb1ffa5cd85
Betinsk <gmdust@hotmail.com>
2020-04-05 19:31:14 -0300

    Urui1015

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
 	
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
+	double x1 = sc.nextDouble();
+	double y1 = sc.nextDouble();
+	double x2 = sc.nextDouble();
+	double y2 = sc.nextDouble();
+	
+	double distancia = Math.sqrt(Math.pow((x2-x1), 2) + (Math.pow(y2-y1, 2)));
 		
-		int maiorAB= (A + B + Math.abs(A-B)) / 2;
-		
-		if (maiorAB> C) {
-			System.out.printf("%d eh o maior\n", maiorAB);
-		}
-		else {
-			System.out.printf("%d eh o maior\n", C);
-		}
-		
-		
+	System.out.printf("%.4f%n", distancia);
 	}
 }
